8449f5dfff1d43a4a053c6deb483fa306a004388
Ramiro Victor <ramirovictor3@gmail.com>
2021-10-18 09:43:43 -0300

    add files via upload

---
+++ 24-spring-data-rest-cruddemo/target/classes/scripts/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO EMPLOYEE VALUES 
+	(1,'Ramiro','Andrews','ramiro@luv2code.com'),
+	(2,'Victor','Baumgarten','victor@luv2code.com'),
+	(3,'Ramires','Gupta','ramires@luv2code.com'),
+	(4,'Vitor','Petrov','vitor@luv2code.com'),
+	(5,'Jose','Vega','jose@luv2code.com');
